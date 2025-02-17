7df19444696af14e5b8d11aa0917ff2ff33dc1f8
btwiamsujal <sujalfaganiya2005@gmail.com>
2025-02-17 14:09:20 +0530

    ses-hjk file modified

--- ses-hjk.py
+++ ses-hjk.py
@@ -6,7 +6,7 @@ target_url = 'http://example.com'   # Target URL you can do whatever you want wi
 """
     Please Don't misuse this if you do it will be a crime and you all usage is recorded in log file of mine so don't even think about it
     Also coping code and paste in other file won't help so don't do it
-    If you want to get full access cpode ethically contact me at my email id: sujalfaganiya2005@gmail.com / sujalpatel1709@gmail.com
+    If you want to get full access code ethically contact me at my email id: sujalfaganiya2005@gmail.com / sujalpatel1709@gmail.com
 """
 
 """_
